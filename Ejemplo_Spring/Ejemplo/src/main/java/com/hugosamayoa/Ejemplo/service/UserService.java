8d44cde9091521e14ad048c82d59e49e35f0eeae
hsamayoa-2021462 <hsamayoa-2021462@kinal.edu.gt>
2025-09-10 11:57:53 -0600

    add: Agregando ejercicio1 con su base de datos

---
+++ Ejemplo_Spring/Ejemplo/src/main/java/com/hugosamayoa/Ejemplo/service/UserService.java
@@ -0,0 +1,15 @@
+package com.hugosamayoa.Ejemplo.service;
+
+import com.hugosamayoa.Ejemplo.model.User;
+
+import java.util.List;
+
+public interface UserService {
+
+    List<User> getAllUsers();
+    User getUserById(Integer id);
+    User saveUser(User user);
+    User updateUser(Integer id, User user);
+    void deleteUser(Integer id);
+
+}
---
+++ Ejemplo_Spring/Ejemplo/src/test/java/com/hugosamayoa/Ejemplo/EjemploUnoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hugosamayoa.Ejemplo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjemploUnoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
